1a42a8254ee4429ac757ba4c05f18b86f37187fb
Gisbert Amm <gisbert.amm@web.de>
2015-08-18 15:55:01 +0200

    first working draft of proposal sending

--- app/src/main/java/kfzkennzeichen/gisbertamm/de/kfz_kennzeichen/ResultFragment.java
+++ app/src/main/java/kfzkennzeichen/gisbertamm/de/kfz_kennzeichen/ResultFragment.java
@@ -1,16 +1,38 @@
 package kfzkennzeichen.gisbertamm.de.kfz_kennzeichen;
 
+import android.app.AlertDialog;
 import android.app.Fragment;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.List;
 
 import kfzkennzeichen.gisbertamm.de.kfz_kennzeichen.persistence.DatabaseHandler;
@@ -39,10 +61,11 @@ public class ResultFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_result, container, false);
 
-        TextView code = (TextView) view.findViewById(R.id.result_code);
+        final TextView code = (TextView) view.findViewById(R.id.result_code);
         TextView district = (TextView) view.findViewById(R.id.result_district);
         TextView districtCenter = (TextView) view.findViewById(R.id.result_district_center);
         TextView jokes = (TextView) view.findViewById(R.id.result_jokes);
+        Button proposeJoke = (Button) view.findViewById(R.id.button_propose_joke);
 
         final SavedEntry savedEntry = getSavedEntry();
 
@@ -70,6 +93,30 @@ public class ResultFragment extends Fragment {
             code.setText(getString(R.string.nothing_found));
         }
 
+        proposeJoke.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Set an EditText view to get user input
+                final EditText input = new EditText(getActivity());
+
+                new AlertDialog.Builder(getActivity())
+                        .setTitle("Eigenen Text vorschlagen")
+                        .setMessage("Bitte Text eingeben (maximal 30 Zeichen).")
+                        .setView(input)
+                        .setPositiveButton("Vorschlagen", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                MailProposalTask proposalTask = new MailProposalTask();
+                                proposalTask.execute(code.getText().toString(), input.getText().toString());
+                            }
+                        })
+                        .setNegativeButton("Abbrechen", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                // Do nothing.
+                            }
+                        }).show();
+            }
+        });
+
         return view;
     }
 
@@ -80,5 +127,78 @@ public class ResultFragment extends Fragment {
             jokesBuff.append("\n");
         }
         return jokesBuff.toString();
+    }
+
+    private class MailProposalTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            HttpClient httpClient = new DefaultHttpClient();
+
+            HttpPost httpPost = new HttpPost("https://api.mailgun.net/v3/sandbox47fa9b0a752440c794641c362d468402.mailgun.org/messages");
+
+            String base64EncodedCredentials = "Basic " + Base64.encodeToString(
+                    ("api" + ":" + "").getBytes(),
+                    Base64.NO_WRAP);
+
+
+            httpPost.setHeader("Authorization", base64EncodedCredentials);
+
+            List<NameValuePair> nameValuePairList = new ArrayList<NameValuePair>();
+            BasicNameValuePair fromBasicNameValuePair = new BasicNameValuePair("from", "In-App-Proposal <kfz-kennzeichen-spruch-vorschlag@web.de>");
+            nameValuePairList.add(fromBasicNameValuePair);
+            BasicNameValuePair toBasicNameValuePAir = new BasicNameValuePair("to", "kfz-kennzeichen-spruch-vorschlag@web.de");
+            nameValuePairList.add(toBasicNameValuePAir);
+            BasicNameValuePair subjectBasicNameValuePair = new BasicNameValuePair("subject", "Neuer Vorschlag für Kennzeichen-Joke (Android)");
+            nameValuePairList.add(subjectBasicNameValuePair);
+            BasicNameValuePair textBasicNameValuePAir = new BasicNameValuePair("text", "code: " + params[0] + ", Vorschlag: " + params[1]);
+            nameValuePairList.add(textBasicNameValuePAir);
+
+            try {
+                UrlEncodedFormEntity urlEncodedFormEntity = new UrlEncodedFormEntity(nameValuePairList);
+
+                httpPost.setEntity(urlEncodedFormEntity);
+
+                try {
+                    HttpResponse httpResponse = httpClient.execute(httpPost);
+
+                    InputStream inputStream = httpResponse.getEntity().getContent();
+
+                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+
+                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                    StringBuilder stringBuilder = new StringBuilder();
+
+                    String bufferedStrChunk = null;
+
+                    while ((bufferedStrChunk = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(bufferedStrChunk);
+                    }
+
+                    return stringBuilder.toString();
+
+                } catch (ClientProtocolException cpe) {
+                    System.out.println("First Exception caz of HttpResponese :" + cpe);
+                    cpe.printStackTrace();
+                } catch (IOException ioe) {
+                    System.out.println("Second Exception caz of HttpResponse :" + ioe);
+                    ioe.printStackTrace();
+                }
+
+            } catch (UnsupportedEncodingException uee) {
+                System.out.println("An Exception given because of UrlEncodedFormEntity argument :" + uee);
+                uee.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            Toast.makeText(getActivity().getApplicationContext(), result, Toast.LENGTH_LONG).show();
+        }
     }
 }
