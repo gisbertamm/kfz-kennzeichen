fb6f63f7177cea2a4a55d4338635bc4ac33a58fb
Gisbert Amm <gisbert.amm@web.de>
2015-08-25 11:49:39 +0200

    do not show empty menu

--- app/src/main/java/kfzkennzeichen/gisbertamm/de/kfz_kennzeichen/SearchActivity.java
+++ app/src/main/java/kfzkennzeichen/gisbertamm/de/kfz_kennzeichen/SearchActivity.java
@@ -40,7 +40,7 @@ public class SearchActivity extends ActionBarActivity implements OnSearchComplet
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_search, menu);
+        // getMenuInflater().inflate(R.menu.menu_search, menu);
         return true;
     }
 
