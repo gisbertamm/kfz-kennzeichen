06f31af9e921d324caf7fd41aafad6dc022798de
Gisbert Amm <gisbert.amm@web.de>
2015-09-08 14:34:17 +0200

    released version 1.0.3 (version code 4)

--- app/src/main/java/kfzkennzeichen/gisbertamm/de/kfz_kennzeichen/persistence/DatabaseHandler.java
+++ app/src/main/java/kfzkennzeichen/gisbertamm/de/kfz_kennzeichen/persistence/DatabaseHandler.java
@@ -17,7 +17,7 @@ import java.util.List;
 
 
 public class DatabaseHandler extends SQLiteOpenHelper {
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 3;
 
     private static final String DATABASE_NAME = "NumberplateCodesManager.sqlite";
 
