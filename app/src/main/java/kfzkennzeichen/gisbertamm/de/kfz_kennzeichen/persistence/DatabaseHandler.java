5e7ad2ab433a43c3cd77084ff1c3089f5c88170c
Gisbert Amm <gisbert.amm@web.de>
2016-08-17 12:58:10 +0200

    added more jokes

--- app/src/main/java/kfzkennzeichen/gisbertamm/de/kfz_kennzeichen/persistence/DatabaseHandler.java
+++ app/src/main/java/kfzkennzeichen/gisbertamm/de/kfz_kennzeichen/persistence/DatabaseHandler.java
@@ -17,7 +17,7 @@ import java.util.List;
 
 
 public class DatabaseHandler extends SQLiteOpenHelper {
-    private static final int DATABASE_VERSION = 6;
+    private static final int DATABASE_VERSION = 7;
 
     private static final String DATABASE_NAME = "NumberplateCodesManager.sqlite";
 
