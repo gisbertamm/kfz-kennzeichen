2d7a79787acb47e86506f07fe4597f933735d432
Gisbert Amm <gisbert.amm@posteo.de>
2020-09-19 00:17:59 +0200

    added new database to the app

--- app/src/main/java/kfzkennzeichen/gisbertamm/de/kfz_kennzeichen/persistence/DatabaseHandler.java
+++ app/src/main/java/kfzkennzeichen/gisbertamm/de/kfz_kennzeichen/persistence/DatabaseHandler.java
@@ -17,7 +17,7 @@ import java.util.List;
 
 
 public class DatabaseHandler extends SQLiteOpenHelper {
-    private static final int DATABASE_VERSION = 8;
+    private static final int DATABASE_VERSION = 9;
 
     private static final String DATABASE_NAME = "NumberplateCodesManager.sqlite";
 
